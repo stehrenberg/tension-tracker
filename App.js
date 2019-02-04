22cbe29fa445451df0c480e6e8b625e6de996a06
Stephanie Ehrenberg <sehrenbe@hm.edu>
2019-02-04 15:32:11 +0100

    start color with black: 0,0.0

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const app = class App extends React.Component {
         super(props);
 
         this.state = {
-            red: 127,
+            red: 0,
             green: 0,
             blue: 0,
         };
